30592fa53d7a345d49580a05d0f5b35c8a461a37
Alireza Yahyapour <lnxpylnxpy@gmail.com>
2019-12-10 06:51:32 -0800

    local settings added

--- postern/settings.py
+++ postern/settings.py
@@ -119,3 +119,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
+
+try:
+    from .local_settings import *
+except ImportError:
+    print('CCCCCCCCCCCCCCc')
